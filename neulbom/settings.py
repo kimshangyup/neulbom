b7830280eae5f83de9c71e1bd84cafc973e3aa50
kimshangyup <kmn1120@naver.com>
2025-11-10 18:11:44 +0900

    2

--- neulbom/settings.py
+++ neulbom/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-ow8+(id6tunud^7!=fyjb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
